efd9a618f11edf2a39134f6dc8901976b94e39a0
Yosmel Chiang <yosmelchiang@gmail.com>
2026-04-01 11:25:03 +0200

    fix: video link

--- src/App.jsx
+++ src/App.jsx
@@ -13,55 +13,55 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Navbar sticky={true} style={{minHeight: "70px"}}>
+      <BrowserRouter>
+        <Navbar style={{ minHeight: "70px" }}>
 
-        <NavLink asListItem={false} tag={Link} to="/">
-          <img style={ { width: "auto", height: "50px", objectFit: "contain"}} src="https://i.imgur.com/UlFsk3d.png" />
-        </NavLink>
-          
-        <NavItems>
-          <NavLink tag={Link} to="/" >Home</NavLink>
-          <Divider direction="vertical"/>
-          <NavLink onClick={() => setLoginOpen(true)}>Login</NavLink>
-          <NavLink onClick={() => setCartOpen(true)}>Cart</NavLink>
-        </NavItems>
-      </Navbar>
+          <NavLink asListItem={false} tag={Link} to="/">
+            <img style={{ width: "auto", height: "110px", objectFit: "contain" }} src="https://i.imgur.com/UlFsk3d.png" />
+          </NavLink>
 
-      <Navbar sticky={true} style={{ top: "70px"}} order="bottom">
-        <NavItems>
-          <NavLink tag={Link} to="/products" >Products</NavLink>
-          <NavLink>Brands</NavLink>
-          <NavLink>Campaigns</NavLink>
-        </NavItems>
-        <NavItems>
-          <NavLink tag={Link} to="/customerservice" >Customer Service</NavLink>
-        </NavItems>
-      </Navbar>
+          <NavItems>
+            <NavLink tag={Link} to="/" >Home</NavLink>
+            <Divider direction="vertical" />
+            <NavLink onClick={() => setLoginOpen(true)}>Login</NavLink>
+            <NavLink onClick={() => setCartOpen(true)}>Cart</NavLink>
+          </NavItems>
+        </Navbar>
 
-      <Routes>
+        <Navbar sticky={true} order="bottom">
+          <NavItems>
+            <NavLink tag={Link} to="/products" >Products</NavLink>
+            <NavLink>Brands</NavLink>
+            <NavLink>Campaigns</NavLink>
+          </NavItems>
+          <NavItems>
+            <NavLink tag={Link} to="/customerservice" >Customer Service</NavLink>
+          </NavItems>
+        </Navbar>
+
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/customerservice" element={<CustomerService />} />
-      </Routes>
+        </Routes>
 
-      <Drawer isOpen={cartOpen} onClose={() => setCartOpen(false)} title="Cart">
-        <p>Products in cart</p>
-        <br />
-        <Button onClick={() => setCartOpen(false)}>Checkout</Button>
-      </Drawer>
-
-      <Modal isOpen={loginOpen} onClose={() => setLoginOpen(false)} title="Login">
-        <Container>
-          <p>Username</p>
-          <Input type="text" placeholder="username"/>
-          <p>Password</p>
-          <Input type="password" placeholder="Password"/>
+        <Drawer isOpen={cartOpen} onClose={() => setCartOpen(false)} title="Cart">
+          <p>Products in cart</p>
           <br />
-          <Button onClick={() => setLoginOpen(false)}>Login</Button>
-        </Container>
-      </Modal>
-    </BrowserRouter>
+          <Button onClick={() => setCartOpen(false)}>Checkout</Button>
+        </Drawer>
+
+        <Modal isOpen={loginOpen} onClose={() => setLoginOpen(false)} title="Login">
+          <Container>
+            <p>Username</p>
+            <Input type="text" />
+            <p>Password</p>
+            <Input type="password" />
+            <br />
+            <Button onClick={() => setLoginOpen(false)}>Login</Button>
+          </Container>
+        </Modal>
+      </BrowserRouter>
     </>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,31 @@
 import { ProductCard, Container } from '@yosang/react-ui'
 
-export default function App () {
+export default function App() {
     const data = [
-        { 
-            image:"https://www.fjellsport.no/assets/blobs/vj-ultra3-lime-1-8d0494608e.jpeg?preset=medium",
-            title:"VJ Sport",
+        {
+            image: "https://www.fjellsport.no/assets/blobs/vj-ultra3-lime-1-8d0494608e.jpeg?preset=medium",
+            title: "VJ Sport",
             description: "Ultra 3.1 Lime",
             price: "2 700",
             discount: "25"
         },
-        { 
-            image:"https://www.fjellsport.no/assets/blobs/jq4441-1-footwear-photography-side-lateral-center-view-white-1e99ef5095.jpeg?preset=medium",
-            title:"Adidas Adizero",
+        {
+            image: "https://www.fjellsport.no/assets/blobs/jq4441-1-footwear-photography-side-lateral-center-view-white-1e99ef5095.jpeg?preset=medium",
+            title: "Adidas Adizero",
             description: "Evo Sl Woven",
             price: "1 749",
             discount: "25"
         },
-        { 
-            image:"https://www.fjellsport.no/assets/blobs/s21070-141-1-0b3c7ef798.jpeg?preset=medium",
-            title:"Saucony Endorphin",
+        {
+            image: "https://www.fjellsport.no/assets/blobs/s21070-141-1-0b3c7ef798.jpeg?preset=medium",
+            title: "Saucony Endorphin",
             description: "Azura M Sage/laurel",
             price: "2 000",
             discount: "25"
         },
-        { 
-            image:"https://www.fjellsport.no/assets/blobs/vj-ultra3-lime-1-8d0494608e.jpeg?preset=medium",
-            title:"VJ Sport",
+        {
+            image: "https://www.fjellsport.no/assets/blobs/vj-ultra3-lime-1-8d0494608e.jpeg?preset=medium",
+            title: "VJ Sport",
             description: "Ultra 3.1 Lime",
             price: "2700",
             discount: "25"
@@ -33,21 +33,21 @@ export default function App () {
     ]
     return (
         <>
-        <Container style={{ width: "80%", margin: "auto"}} centered={true}>
-        <video
-            src="https://cdn.jumpshare.com/preview/gRWiHgcGHQA_L90A_28rUwl244jROwYmlu9lzxcy8S5VP3AQSPz2U9EupbfElYMQgH8og0a99jYUDe5Mz6b0NR_-BMMe_-VmMnFquZua754171UBOQvLOiAKbaIZ-SvWwpjCRp12Y-gfClUOQokSNG6yjbN-I2pg_cnoHs_AmgI.mp4"  // or an external URL
-            autoPlay
-            loop       // loops the video continuously
-            muted      // must be muted to autoplay on most browsers
-            playsInline // ensures autoplay works on mobile Safari
-            style={{ width: "100%", height: "450px", objectFit: "cover" }} // responsive
-        />
-        </Container>
-        <Container layout="grid" style={{ width: "80%", margin: "auto"}} centered={true}>
-            {data.map(p => (
-                <ProductCard discount={p.discount} image={p.image} title={p.title} description={p.description} price={p.price} />
-            ))}
-        </Container>
+            <Container style={{ width: "80%", margin: "auto" }} centered={true}>
+                <video
+                    src="https://i.imgur.com/5PUkZRJ.mp4"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline // ensures autoplay works on mobile Safari
+                    style={{ width: "100%", height: "450px", objectFit: "cover" }} // responsive
+                />
+            </Container>
+            <Container layout="grid" style={{ width: "80%", margin: "auto" }} centered={true}>
+                {data.map(p => (
+                    <ProductCard discount={p.discount} image={p.image} title={p.title} description={p.description} price={p.price} />
+                ))}
+            </Container>
         </>
     )
 }
